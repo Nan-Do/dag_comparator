3f962c92a1c0d3d50ec32f74193d59f0fe1bccc3
Fernando Tarin Morales <icemanf@gmail.com>
2018-01-05 19:00:19 +0900

    Refactored the code to be in the class DirectedAcyclicGraph and updated the tests to run against the class.

--- test-enumerate.py
+++ test-enumerate.py
@@ -2,8 +2,7 @@ import unittest
 
 from collections import defaultdict
 
-from enumerate import generateVariableCombinations
-from enumerate import buildSuccessors
+from directed_acyclic_graph import DirectedAcyclicGraph
 
 
 class testGenerateVariableCombinations(unittest.TestCase):
@@ -15,59 +14,52 @@ class testGenerateVariableCombinations(unittest.TestCase):
         #   |  b-c
         #   |  | |
         #    --d e
-        self.root_1 = "a"
-        self.graph_1 = {
+        root = "a"
+        graph = {
             "a": tuple("bcd"),
             "b": tuple("cd"),
             "c": tuple("e"),
             "d": tuple(""),
             "e": tuple("")
         }
+        self.dag1 = DirectedAcyclicGraph(root, graph)
 
         # Trivial example
         #       a
         #      / \
         #      b c
-        self.root_2 = "a"
-        self.graph_2 = {
+        root = "a"
+        graph = {
             "a": tuple("bc"),
             "b": tuple(""),
             "c": tuple("")
         }
+        self.dag2 = DirectedAcyclicGraph(root, graph)
 
     # Tests to validate the input
-    def test_SetVariableEmtpyGraph(self):
-        self.assertRaises(ValueError,
-                          generateVariableCombinations,
-                          self.root_1,
-                          dict(),
-                          2)
-
     def test_SetVariableUnknownRoot(self):
         self.assertRaises(ValueError,
-                          generateVariableCombinations,
+                          self.dag1.generateVariableCombinations,
                           "z",
-                          self.graph_1,
                           2)
 
     def test_SetVariableIncorrectVariables(self):
         self.assertRaises(ValueError,
-                          generateVariableCombinations,
+                          self.dag1.generateVariableCombinations,
                           "z",
-                          self.graph_1,
                           0)
 
     # Tests to check that the solutions are correct
     def test_Set1VariableGraph1(self):
         valid_solution = set([('c',), ('d',), ('b',), ('e',)])
-        solution = generateVariableCombinations(self.root_1, self.graph_1, 1)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root, 1)
 
         self.assertEqual(valid_solution, solution)
 
     def test_Set2VariablesGraph1(self):
         valid_solution = set([('d', 'e'), ('c',), ('b',), ('b', 'c'), ('e',),
                               ('d',), ('c', 'd'), ('b', 'e'), ('b', 'd')])
-        solution = generateVariableCombinations(self.root_1, self.graph_1, 2)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root, 2)
 
         self.assertEqual(valid_solution, solution)
 
@@ -76,17 +68,16 @@ class testGenerateVariableCombinations(unittest.TestCase):
                               ('b', 'c'), ('e',), ('d',), ('b', 'd', 'e'),
                               ('c', 'd'), ('b', 'e'), ('b', 'd')])
 
-        solution = generateVariableCombinations(self.root_1, self.graph_1, 3)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root, 3)
 
         self.assertEqual(valid_solution, solution)
 
     def test_Set4VariableGraph1(self):
         # This should be the same as for 3 variables as you can put 4 variables
         # on the graph
-        valid_solution = generateVariableCombinations(self.root_1,
-                                                      self.graph_1,
-                                                      3)
-        solution = generateVariableCombinations(self.root_1, self.graph_1, 4)
+        valid_solution = self.dag1.generateVariableCombinations(self.dag1.root,
+                                                                3)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root, 4)
 
         self.assertEqual(valid_solution, solution)
 
@@ -94,45 +85,41 @@ class testGenerateVariableCombinations(unittest.TestCase):
     # more variables than the ones we specified on the parameter list
     def test_Set1VariableDoesntGenerate2orMoreVariablesGraph1(self):
         num_variables = 1
-        solution = generateVariableCombinations(self.root_1,
-                                                self.graph_1,
-                                                num_variables)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
     def test_Set2VariablesDoesntGenerate3orMoreVariablesGraph1(self):
         num_variables = 2
-        solution = generateVariableCombinations(self.root_1,
-                                                self.graph_1,
-                                                num_variables)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
     def test_Set3VariablesDoesntGenerate4orMoreVariablesGraph1(self):
         num_variables = 3
-        solution = generateVariableCombinations(self.root_1,
-                                                self.graph_1,
-                                                num_variables)
+        solution = self.dag1.generateVariableCombinations(self.dag1.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
     def test_Set1VariableGraph2(self):
         valid_solution = set([('b',), ('c',)])
-        solution = generateVariableCombinations(self.root_2, self.graph_2, 1)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root, 1)
 
         self.assertEqual(valid_solution, solution)
 
     def test_Set2VariableGraph2(self):
         valid_solution = set([('b',), ('c',), ('b', 'c')])
-        solution = generateVariableCombinations(self.root_2, self.graph_2, 2)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root, 2)
 
         self.assertEqual(valid_solution, solution)
 
     def test_Set3VariableGraph2(self):
-        valid_solution = generateVariableCombinations(self.root_2,
-                                                      self.graph_2,
-                                                      2)
-        solution = generateVariableCombinations(self.root_2, self.graph_2, 3)
+        valid_solution = self.dag2.generateVariableCombinations(self.dag2.root,
+                                                                2)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root, 3)
 
         self.assertEqual(valid_solution, solution)
 
@@ -140,25 +127,22 @@ class testGenerateVariableCombinations(unittest.TestCase):
     # more variables than the ones we specified on the parameter list
     def test_Set1VariableDoesntGenerate2orMoreVariablesGraph2(self):
         num_variables = 1
-        solution = generateVariableCombinations(self.root_2,
-                                                self.graph_2,
-                                                num_variables)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
     def test_Set2VariablesDoesntGenerate3orMoreVariablesGraph2(self):
         num_variables = 2
-        solution = generateVariableCombinations(self.root_2,
-                                                self.graph_2,
-                                                num_variables)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
     def test_Set3VariablesDoesntGenerate4orMoreVariablesGraph2(self):
         num_variables = 3
-        solution = generateVariableCombinations(self.root_2,
-                                                self.graph_2,
-                                                num_variables)
+        solution = self.dag2.generateVariableCombinations(self.dag2.root,
+                                                          num_variables)
         solution = all(map(lambda x: len(x) <= num_variables, solution))
         self.assertTrue(solution)
 
@@ -169,28 +153,33 @@ class testBuildSuccessors(unittest.TestCase):
         #       a
         #      / \
         #      b c
-        self.root_2 = "a"
-        self.graph_2 = {
+        root = "a"
+        graph = {
             "a": tuple("bc"),
             "b": tuple(""),
             "c": tuple("")
         }
+        self.dag = DirectedAcyclicGraph(root, graph)
 
     def test_successorsFromRootGraph2(self):
         solutions = {'a': {'c': 1, 'b': 1}}
         successors = defaultdict(dict)
-        buildSuccessors(self.root_2, self.graph_2, 0, set(), successors)
+        self.dag._DirectedAcyclicGraph__buildSuccessors(self.dag.root,
+                                                        0,
+                                                        set(),
+                                                        successors)
 
         self.assertEqual(successors, solutions)
 
     def test_successorsFromLeafGraph2(self):
         solutions = {}
         successors = defaultdict(dict)
-        buildSuccessors('b', self.graph_2, 0, set(), successors)
+        self.dag._DirectedAcyclicGraph__buildSuccessors('b',
+                                                        0,
+                                                        set(),
+                                                        successors)
 
         self.assertEqual(successors, solutions)
-
-
 
 
 if __name__ == '__main__':
