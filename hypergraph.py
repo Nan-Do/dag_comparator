d0a97de00e11c3f84e4bce570ba1e71bca29bc61
Fernando Tarin Morales <icemanf@gmail.com>
2018-01-12 14:52:42 +0900

    Fully changed the implementattion of the hypergraph to match the current requirements

--- hypergraph.py
+++ hypergraph.py
@@ -1,54 +1,121 @@
-from collections import namedtuple
+from collections import defaultdict
 
-# This will represent a node of the hypergraph
-# next -> A dictionary representing the next level
-# data -> The data for the node
-Node = namedtuple('node', ['next', 'value'])
+
+class NodeData:
+    def __init__(self, value, hyperedges):
+        self.value = value
+        self.hyperedges = hyperedges
 
 
 class Hypergraph:
+    """
+    This class represents  a simple hypergraph
+    It functions by specifying the nodes that forms the hypergraph and
+    then the edges.
+    So far it is quite simple and only adding functions have been
+    implemented but the algorithim doesn't require to remove any data
+    Both the nodes and the hyperedges can be labeled with data.
+    """
+
     def __init__(self):
-        self.root = dict()
+        self.last_hyperedge = 0
+        self.nodes = defaultdict(NodeData)
+        self.hyperedges = dict()
+        self.positions_to_hyperedges = dict()
 
-    def __get_level_dictionary(self, nodes):
-        current = self.root
-        for node in nodes[:-1]:
-            if node not in current:
-                return None
-
-            current = current[node].next
-        return current
-
-    def addValue(self, nodes, value):
+    def addNode(self, node, value):
         """
-        Adds the value to the hypergraph following the sequence of nodes.
-
-        Nodes must be a list of nodes that will be queried, if a node doesn't
-        exist on the hypergraph it will be created, if it is not the last one
-        on the sequence it will raise an error.
-        The data for the nodes must be inmutable (set, strings, etc...)
+        This function adds a node to the hypergraph.
+        If the node already exists it updates its value
+        Node -> The node of the hypergraph must be inmutable.
+        Value -> The value associated with the node.
         """
+        if node not in self.nodes:
+            self.nodes[node] = NodeData(value, list())
+        else:
+            self.nodes[node].value = value
 
-        d = self.__get_level_dictionary(nodes)
-        if d is None:
-            raise ValueError("Requested to add an invalid path on the " +
-                             "hypergraph as one of it is itermediate" +
-                             "nodes doesn't exist")
-
-        new_node = Node(dict(), value)
-        d[nodes[-1]] = new_node
-
-    def getValue(self, nodes):
+    def updateNode(self, node, value):
         """
-        Returns the value associated with a node in the hypergraph.
-
-        If the sequence of nodes doesn't exist it will return None.
-        Nodes must be a list of nodes
+        This functions updates the value associated with a node of
+        the hypergraph. If the node doesn't exists raises an exception
+        Node -> The node of the hypergraph must be inmutable.
+        Value -> The value associated with the node.
         """
+        if node not in self.nodes:
+            raise ValueError("The node doesn't exists on the hypergraph")
 
-        d = self.__get_level_dictionary(nodes)
+        self.nodes[node].value = value
 
-        if d is None or nodes[-1] not in d:
-            return None
+    def getNodeValue(self, node):
+        """
+        This function returns the value associated with a node. If the node
+        doesn't exists raises an exception
+        Node -> The node of the hypergraph must be inmutable.
+        Value -> The value associated with the node.
+        """
+        if node not in self.nodes:
+            raise ValueError("The node doesn't exists on the hypergraph")
 
-        return d[nodes[-1]].value
+        return self.nodes[node].value
+
+    def addHyperedge(self, hyperedge, label):
+        """
+        This function adds a hyperedges to the hypergraph. If it already exists
+        it updates its associated label.
+        hyperedges -> a tuple of nodes if a node doesn't exists it raises an
+                      exception.
+        label -> The data associated with the hyperedges.
+        """
+        if hyperedge not in self.hyperedges:
+            current_position = self.last_hyperedge
+            self.last_hyperedge += 1
+
+            for node in hyperedge:
+                if node not in self.nodes:
+                    raise ValueError("The hyperedge contains a node that " +
+                                     "doesn't exist on the hypergraph")
+
+                self.nodes[node].hyperedges.append(current_position)
+
+            self.positions_to_hyperedges[current_position] = hyperedge
+
+        self.hyperedges[hyperedge] = label
+
+    def getHyperedgeLabel(self, hyperedge):
+        """
+        This function returns the label associated with a hyperedge of the
+        hyperedge. If it doesn't exists it raises an exception
+        hyperedges -> a tuple of nodes if a node doesn't exists it raises an
+                      exception.
+        """
+        if hyperedge not in self.hyperedges:
+            raise ValueError("The hyperedge doesn't exists on the hypergraph")
+
+        return self.hyperedges[hyperedge]
+
+    def updateHyperedgeLabel(self, hyperedge, label):
+        """
+        This function updates the label associated with a hyperedge of the
+        hyperedge. If it doesn't exists it raises an exception
+        hyperedges -> a tuple of nodes if a node doesn't exists it raises an
+                      exception.
+        """
+        if hyperedge not in self.hyperedges:
+            raise ValueError("The hyperedge doesn't exists on the hypergraph")
+
+        self.hyperedges[hyperedge] = label
+
+    def getHyperedgesFromNode(self, node):
+        """
+        This function returns the hyperedges that a node belongs to. If the
+        node doesn't exist on the hypergraph it raises an exception.
+        """
+        if node not in self.nodes:
+            raise ValueError("The node doesn't exists on the hypergraph")
+
+        hyperedges = list()
+        for hyperedge_position in self.nodes[node].hyperedges:
+            hyperedges.append(self.positions_to_hyperedges[hyperedge_position])
+
+        return hyperedges
--- test-hypergraph.py
+++ test-hypergraph.py
@@ -4,49 +4,112 @@ from hypergraph import Hypergraph
 
 
 class TestHypergraph(unittest.TestCase):
-    def test_addValueError(self):
-        a = Hypergraph()
-        nodes = [1, 2]
+    def setUp(self):
+        self.a = Hypergraph()
 
+    def test_checkUnknownNode(self):
         self.assertRaises(ValueError,
-                          a.addValue,
-                          nodes,
-                          0)
+                          self.a.updateNode,
+                          "z",
+                          2)
 
-    def test_addValueSimple(self):
-        a = Hypergraph()
+    def test_checkUnknownHyperedge(self):
+        self.assertRaises(ValueError,
+                          self.a.updateHyperedgeLabel,
+                          ("z", "s"),
+                          2)
 
-        nodes = [1]
-        a.addValue(nodes, 1)
+    def test_addNode1(self):
+        self.a.addNode('a', 1)
 
-    def test_addValue2(self):
-        a = Hypergraph()
+        self.assertEqual(self.a.getNodeValue('a'), 1)
 
-        nodes = [1]
-        a.addValue(nodes, 1)
-
-        nodes = [1, 2]
-        a.addValue(nodes, 2)
-
-    def test_getValueNonExisting(self):
-        a = Hypergraph()
-
-        nodes = [1, 2]
-        res = a.getValue(nodes)
-
-        self.assertEquals(res, None)
-
-    def test_getValueExisting(self):
-        a = Hypergraph()
-
-        nodes = [1]
-        a.addValue(nodes, 1)
-
-        nodes = [1, 2]
-        a.addValue(nodes, 2)
+    def test_addNode2(self):
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
         
-        res = a.getValue(nodes)
-        self.assertEqual(res, 2)
+        self.assertEqual(self.a.getNodeValue('a'), 1)
+        self.assertEqual(self.a.getNodeValue('b'), 2)
+ 
+    def test_addNode3(self):
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+        self.a.addNode('c', 3)
+        
+        self.assertEqual(self.a.getNodeValue('a'), 1)
+        self.assertEqual(self.a.getNodeValue('b'), 2)
+        self.assertEqual(self.a.getNodeValue('c'), 3)
+        
+    def test_updateNodeValue(self):
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+
+        self.a.updateNode('a', 3)
+
+        self.assertEqual(self.a.getNodeValue('a'), 3)
+        
+    def test_addHyperedge1(self):
+        he = ('a', 'b', 'c')
+
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+        self.a.addNode('c', 3)
+        
+        self.a.addHyperedge(he, "abc")
+
+        self.assertEqual(self.a.getHyperedgeLabel(he), "abc")
+ 
+    def test_addHyperedge2(self):
+        he = ('a', 'b', 'c')
+        he2 = ('d', 'b', 'c')
+
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+        self.a.addNode('c', 3)
+        self.a.addNode('d', 4)
+        
+        self.a.addHyperedge(he, "abc")
+        self.a.addHyperedge(he2, "dbc")
+
+        self.assertEqual(self.a.getHyperedgeLabel(he2), "dbc")
+
+
+    def test_updateHyperedgeLabel(self):
+        he = ('a', 'b', 'c')
+        he2 = ('d', 'b', 'c')
+
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+        self.a.addNode('c', 3)
+        self.a.addNode('d', 4)
+        
+        self.a.addHyperedge(he, "abc")
+        self.a.addHyperedge(he2, "dbc")
+
+        self.a.updateHyperedgeLabel(he, "test")
+
+        self.assertEqual(self.a.getHyperedgeLabel(he), "test")
+
+
+
+    def test_checkHyperedgesAndNodes(self):
+        he = ('a', 'b', 'c')
+        he2 = ('d', 'b', 'c')
+        solution = [he, he2]
+
+        self.a.addNode('a', 1)
+        self.a.addNode('b', 2)
+        self.a.addNode('c', 3)
+        self.a.addNode('d', 4)
+        
+        self.a.addHyperedge(he, "abc")
+        self.a.addHyperedge(he2, "dbc")
+
+        self.assertEqual(self.a.getHyperedgesFromNode('b'), solution)
+
+
+ 
+
 
 
 if __name__ == '__main__':
